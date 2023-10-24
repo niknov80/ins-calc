d70d8f9a1aaefcbee09e35b3d4b1fafde287da56
niknov80 <n.novozhilov80@gmail.com>
2023-10-24 13:18:49 +0300

    Сделан счетчик

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
 import React from "react";
+import Counter from "./Counter/Counter";
+
 
 function App() {
-  const [count, setCount] = React.useState(0);
-  const onClickPlus = () => {
-    setCount(count + 1);
-  }
-  const onClickMinus = () => {
-    setCount(count - 1);
-  }
-
   return (
     <div className="App">
-     <div className="wrapper">
-       <h2>Счетчик:</h2>
-       <h1>{count}</h1>
-       <button onClick={onClickMinus} className="minus">- Минус</button>
-       <button onClick={onClickPlus} className="plus">Плюс +</button>
-     </div>
+      <Counter/>
     </div>
   );
 }
